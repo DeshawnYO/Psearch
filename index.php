c657671e1b7e444675c58c58350c28f8998a2990
dreamans <dreamans@163.com>
2014-04-18 15:06:40 +0800

    修改文件名

--- index.php
+++ index.php
@@ -1,24 +1,8 @@
 <?php
 
-/*
- * Psearch [A journey always starts with the first step]
- *
- * @copyright Copyright (C) 2013 wine.cn All rights reserved.
- * @license http://www.apache.org/licenses/LICENSE-2.0.txt
- */
-
-//----------------------------------------------------------------
-
-/**
- * 项目入口文件
- *
- * @author   <mengfk@eswine.com>
- * @since    1.0
- */
-
 if(version_compare(PHP_VERSION,'5.3.0','<') ) {
     die('require PHP 5.3+');
 }
 define("ROOT", dirname(__FILE__));
-include ROOT.'/Inc/AutoLoad.Inc.php';
+include ROOT.'/inc/AutoLoad.php';
 \Psearch\Inc\Core::getInstance()->run();
